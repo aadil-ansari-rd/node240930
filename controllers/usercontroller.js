4072bcd00d3d44e2ff897cf97a4030c0aeffa19a
aadil-ansari-rd <aadilansari.rd@gmail.com>
2024-09-30 12:38:59 +0530

    up

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -1,14 +1,20 @@
+const bcrypt = require('bcrypt');
 const User = require('../models/User');
- async function signUp(req,res){
-    try{
-        let user = new User(req.body) ;
+const saltRounds = 10;
+async function signUp(req, res) {
+    try {
+        
+        let password = bcrypt.hashSync(req.body.password,saltRounds);
+        console.log(password , 'password');
+        let user = new User(req.body);
+        user.password= password;
         await user.save();
-        res.end("<h4>good</h4>")
-    }catch(err){
+        res.redirect('/');//it will tale to the path of login page
+    } catch (err) {
         console.log(err);
     }
- }
+}
 
- module.exports= {
+module.exports = {
     signUp
- }
+}
